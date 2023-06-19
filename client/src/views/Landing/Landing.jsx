04b956ab19a675aec3cdebba889152c1d68c5ff9
gastondvoskin <dvoskingaston@gmail.com>
2023-06-19 17:31:32 -0300

    Created basic components and routes in cliet. WIP

---
+++ client/src/views/Landing/Landing.jsx
@@ -0,0 +1,10 @@
+function Landing() {
+    return (
+        <div>
+            NIY: This is the Landing.
+        </div>
+    )
+};
+  
+export default Landing;
+  
---
+++ client/src/views/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (
+        <div>
+            NIY: This is the NotFound.
+        </div>
+    )
+};
+  
+export default NotFound;
