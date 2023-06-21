8d5f5ea316e555ad62667aa2724411c6511f016a
gastondvoskin <dvoskingaston@gmail.com>
2023-06-21 10:48:20 -0300

    Conflict resolve

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import SearchBar from "../SearchBar/SearchBar.jsx";
-import { Link, /* NavLink */ } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
 import logo from "../../assets/logo.jpeg";
 
