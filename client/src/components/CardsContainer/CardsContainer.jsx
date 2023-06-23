5a01ff5875f30f06a3bd19cb1657891bf437fc3a
Francisco Aleman <franciscoaleman1993@gmail.com>
2023-06-23 00:11:56 -0300

    Componente Home y Paginado

---
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -0,0 +1,22 @@
+import Card from "../Card/Card"
+import style from "./CardsContainer.module.css"
+import { Link } from "react-router-dom";
+
+export default function CardsContainer({currentFoods}) {
+
+    return (
+      <div className={style.cardsContainer}>
+        {
+        currentFoods?.map((c) => {
+          return (
+              <Link key={c.id} to={`/detail/${c.id}`} className={style.link}>
+                <Card name={c.name} image={c.image} final_price={c.final_price} />
+              </Link>
+
+          );
+        })
+        }
+      </div>
+      
+    );
+  }
