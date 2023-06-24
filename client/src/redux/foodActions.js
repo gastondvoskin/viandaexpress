ef063f5b1e484bf75a811a6c8e29afa8396e8605
josevegas <36419038+josevegas@users.noreply.github.com>
2023-06-24 18:54:11 -0500

    integrando cloudinary, hay problemas en el post del back

--- client/src/redux/foodActions.js
+++ client/src/redux/foodActions.js
@@ -1,7 +1,7 @@
 // actions (these are the actions that will be imported in React components)
 
 import axios from "axios";
-import { getAllFoods, getAllFoodsByName,/* , getFoodById */ } from "./foodSlice.js";
+import { getAllFoods, getAllFoodsByName, postFoods/* , getFoodById */ } from "./foodSlice.js";
 
 export const getFoods = () => async (dispatch) => {
     try {
@@ -24,6 +24,27 @@ export const getNameFood = (name) => async (dispatch) => {
     }
 };
 
+export const postFood=(input)=>async (dispatch)=> {
+    try {
+        const formData = new FormData();
+        formData.append("name", input.name);
+        formData.append("description", input.description);
+        formData.append("category", input.category);
+        formData.append("diet", input.diets);
+        formData.append("initial_price", input.initial_price);
+        formData.append("discount", input.discount);
+        formData.append("image", input.image);
+        console.log(formData);
+        await axios.post("http://localhost:3001/food", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+        });
+        dispatch(postFoods());
+        } catch (error) {
+            alert (error.message)
+        }
+}
 
 // Do not delete. Uncomment and test when the endpoint `http://localhost:3001/food/${id}` is created
 // export const getFood = (id) => async (dispatch) => {
