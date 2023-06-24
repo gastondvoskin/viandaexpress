a38d7c971d072520b397f42959bdd824a5235d8e
gastondvoskin <dvoskingaston@gmail.com>
2023-06-24 20:34:14 -0300

    Fixed api endpoint. Uncommented Carousel in Home.jsx.

--- api/src/routes/indexRouter.js
+++ api/src/routes/indexRouter.js
@@ -1,14 +1,20 @@
-const { Router } = require('express');
-const { foodRouter } = require('./foodRouter');
+const { Router } = require("express");
+const { foodRouter } = require("./foodRouter");
 const { foods } = require("../../api");
-const { Food } = require('../db');
+const { Food } = require("../db");
 
 const router = Router();
 
-router.use('/food', foodRouter);
-router.use('/api', async (req, res) => {
-    const apiEnsaldas = await Food.bulkCreate(foods);
+router.use("/food", foodRouter);
+
+router.use("/api", async (req, res) => {
+  const allFoods = await Food.findAll();
+  if (!allFoods.length) {
+    await Food.bulkCreate(foods);
     res.status(200).json("Comidas cargadas");
-})
+  } else {
+    res.status(200).json("Comidas ya están cargadas");
+  }
+});
 
 module.exports = router;
