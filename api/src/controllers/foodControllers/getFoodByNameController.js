cbd05a9674ce7e416c2280fdfafb21015c1384db
Gabriel-682 <116679204+Gabriel-682@users.noreply.github.com>
2023-06-26 16:25:56 -0300

    getFoodByNameController modified

--- api/src/controllers/foodControllers/getFoodByNameController.js
+++ api/src/controllers/foodControllers/getFoodByNameController.js
@@ -1,18 +1,12 @@
-const { getAllFoodController } = require("./getAllFoodsController");
-
-
-function removeAccents(str) {
-    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  }
+const { Op } = require("sequelize");
+const { Food } = require("../../db");
 
 const getFoodByNameController = async (name) => {
-  const allFood = await getAllFoodController();
-  let results = await allFood.filter((food) =>
-    removeAccents(food.name.toLowerCase()).includes(removeAccents(name.toLowerCase()))
-  );
-  return results.length !== 0
-    ? results
-    : []
+  const foodByname = await Food.findAll({
+    where: { name: { [Op.iLike]: `%${name}%` } },
+  });
+
+  return foodByname;
 };
 
 module.exports = { getFoodByNameController };
