22c98839b25845d5739d457bde0c67f9f2607b3e
josevegas <36419038+josevegas@users.noreply.github.com>
2023-06-28 14:54:16 -0500

    antes de actualizar

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const UserFunction = require("./models/User.js"); 
 const ReviewFunction = require("./models/Review.js");
 const FoodFunction = require("./models/Food.js"); 
-const BasketFunction = require("./models/Basket.js");
+const OrderFunction = require("./models/Order.js");
 const DetailFunction = require("./models/Detail.js");
 
 
@@ -17,11 +17,11 @@ const sequelize = new Sequelize(
 UserFunction(sequelize);
 ReviewFunction(sequelize);
 FoodFunction(sequelize);
-BasketFunction(sequelize);
+OrderFunction(sequelize);
 DetailFunction(sequelize);
 
 // ASSOCIATIONS
-const { User, Review, Food, Basket, Detail } = sequelize.models; 
+const { User, Review, Food, Order, Detail } = sequelize.models; 
 User.hasMany(Review);
 Review.belongsTo(User);
 
@@ -33,11 +33,11 @@ User.belongsToMany(Food, {through: "Favorite"});
 Food.belongsToMany(User, {through: "Favorite"});
 
 
-User.hasMany(Basket);
-Basket.belongsTo(User);
+User.hasMany(Order);
+Order.belongsTo(User);
 
-Basket.hasMany(Detail);
-Detail.belongsTo(Basket);
+Order.hasMany(Detail);
+Detail.belongsTo(Order);
 
 Food.hasMany(Detail);
 Detail.belongsTo(Food);
--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('Basket', 
+  sequelize.define('Order', 
     {
       name: {
         type: DataTypes.STRING,
