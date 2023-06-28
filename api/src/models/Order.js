e40e23da32fadb87d1ae6c468191eab2a6b22aca
josevegas <36419038+josevegas@users.noreply.github.com>
2023-06-28 09:36:22 -0500

    diseñando modelo order

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -3,11 +3,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   sequelize.define('Order', 
     {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      }
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        user_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        total_price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        payment_date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        bill: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        pickup_date: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        }
     },
-    {timestamps: false}
+    
   );
 };
