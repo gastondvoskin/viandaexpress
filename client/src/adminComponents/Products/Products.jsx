8b660f845c644079b3ca0dfe45b98fac586fc53e
santilagos <sntlgs@gmail.com>
2023-06-29 18:50:19 -0300

    Limio un poco de codio y y renombro componente productos a Products

--- client/src/adminComponents/Products/Products.jsx
+++ client/src/adminComponents/Products/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ListProductsItem from '../ListProductsItam/ListProductsItem.jsx';
-import styles from './productos.module.css'; // Importa el archivo CSS
+import styles from './products.module.css'; // Importa el archivo CSS
 import { Link } from 'react-router-dom';
 
 const Productos = () => {
