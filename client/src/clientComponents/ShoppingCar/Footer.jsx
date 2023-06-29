73ee14c2dee4168973363570cdf936258f108f9b
josevegas <36419038+josevegas@users.noreply.github.com>
2023-06-29 02:51:20 -0500

    pasarela de pago y carrito

--- client/src/clientComponents/ShoppingCar/Footer.jsx
+++ client/src/clientComponents/ShoppingCar/Footer.jsx
@@ -1,17 +1,16 @@
 import logo from '../../assets/mercadopago/horizontal_logo.png'
-import styles from './ShoppingCar.module.css';
 
 const Footer = () => {
     return (
-      <footer>
-        <div className={styles.footer_logo}>
+      <footer className="footer">
+        <div className="footer_logo">
           <img
-            className={styles.horizontal_logo}
+            id="horizontal_logo"
             alt="image of the logo"
             src={logo}
           />
         </div>
-        <div className={styles.footer_text}>
+        <div className="footer_text">
           <p>Developers Site:</p>
           <p>
             <a href="https://developers.mercadopago.com" target="_blank">
