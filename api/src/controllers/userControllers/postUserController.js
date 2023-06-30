a715a08c84a37a460f6e0b5f462d2b1e818ece09
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2023-06-29 22:26:32 -0300

    Nodemailer implemented in Post User

--- api/src/controllers/userControllers/postUserController.js
+++ api/src/controllers/userControllers/postUserController.js
@@ -1,9 +1,21 @@
+const e = require('express');
 const { User } = require('../../db');
+const sendEmailWelcome = require("../../nodemailer/sendEmail");
 
 
 const postUserController = async (name, email, type, status, adress) => {
+  const userByEmail = await User.findAll({
+    where: { email: email },
+  });
+  if (userByEmail.length) {
+    throw new Error(`El correo electrónico ${email} pertenece a un usuario que ya está registrado.`)
+  };
   const newUser = await User.create({ name, email, type, status, adress });
-  return newUser.dataValues;    /* check this line */
+
+  if (newUser.dataValues) {
+    await sendEmailWelcome(email)
+  };
+  return newUser.dataValues;
 };
 
 
--- api/src/nodemailer/sendEmail.js
+++ api/src/nodemailer/sendEmail.js
@@ -1,42 +1,39 @@
-const nodemailer = require("nodemailer")
-const logo = require("../../../client/src/assets/logo/LogoViandaExpress.jpeg")
+const nodemailer = require("nodemailer");
+// const { User } = require("../db");
+require("dotenv").config();
+const { EMAIL_USER, EMAIL_PASS } = process.env;
 
-const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-        user: "",
-        pass: "",
-    }
-})
-
-const mailSend = {
-    from: "",
-    to: "",
-    subject: "Vianda Express",
-    text: "Bienvenido a Vianda Express",
-    attachments: [
-        {
-            filename: "logo-transparent.png",
-            path: "../../../client/src/assets/logo/LogoViandaExpress.jpeg",
+async function sendEmailWelcome(userEmail) {
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: EMAIL_USER,
+            pass: EMAIL_PASS,
         }
-    ]
-}
+    });
 
-transporter.sendMail(mailSend, (error, info) => {
-    if (error) {
-        console.log("Error al enviar el correo electrónico:", error);
-    } else {
+    const mailSend = {
+        from: EMAIL_PASS,
+        to: userEmail,
+        subject: "Vianda Express",
+        text: "¡Bienvenido a Vianda Express! Gracias por registrarte en nuestra plataforma.",
+    };
+
+    transporter.sendMail(mailSend, (error, info) => {
+        if (error) {
+            console.log("Error al enviar el correo electrónico:", error);
+        } else {
+            console.log("Correo enviado con éxito", info.response);
+        };
+    });
+
+    try {
+        const info = await transporter.sendMail(mailSend);
         console.log("Correo enviado con éxito", info.response);
-    }
-})
+    } catch (error) {
+        console.log("Error al enviar el correo electrónico:", error);
+    };
+};
 
-// Lee la ruta de la imagen del argumento de línea de comandos
-const imagePath = process.argv[2];
+module.exports = sendEmailWelcome;
 
-// Verifica si se proporcionó una ruta de imagen como argumento
-if (imagePath) {
-    // Llama a la función para enviar el correo con la imagen adjunta
-    sendEmailWithImage(imagePath);
-} else {
-    console.log('Por favor, proporciona la ruta de la imagen como argumento.');
-}
