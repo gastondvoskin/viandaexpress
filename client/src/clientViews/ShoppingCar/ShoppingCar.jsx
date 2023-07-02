2f4aadb4c4f230fdfcd85e80eb1cbb00bea001bb
josevegas <36419038+josevegas@users.noreply.github.com>
2023-07-01 22:46:24 -0500

    previo a la actualizacion

--- client/src/clientViews/ShoppingCar/ShoppingCar.jsx
+++ client/src/clientViews/ShoppingCar/ShoppingCar.jsx
@@ -6,6 +6,7 @@ import Footer from "../../clientComponents/ShoppingCar/Footer.jsx";
 import InternalProvider from "../../clientComponents/ShoppingCar/ContextProvider.jsx";
 import { SpinnerCircular } from 'spinners-react';
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 // REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
 initMercadoPago('APP_USR-8e95f5fd-f2e0-4982-8ac8-27b1f1b175bb');
@@ -14,12 +15,18 @@ initMercadoPago('APP_USR-8e95f5fd-f2e0-4982-8ac8-27b1f1b175bb');
 const ShoppingCar = () => {
   const [preferenceId, setPreferenceId] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [orderData, setOrderData] = useState([
-    {id:1,name:'producto1',image:'img1',finaly_price:10,quantity:1,amount:10},
-    {id:2,name:'producto2',image:'img2',finaly_price:10,quantity:1,amount:10},
-    {id:3,name:'producto3',image:'img3',finaly_price:10,quantity:1,amount:10},
-  ]);
-    
+  const items=useSelector(state=>state.foods.orderItems)
+  const [orderData, setOrderData] = useState([]);
+  // useEffect(()=>{
+  //   if(!items.length){
+  //     axios.get('http://localhost:3001/shopping-cart')
+  //   }
+  // })
+  console.log(items)
+  items.map(it=>{
+    console.log(it)
+    setOrderData([...orderData,it])
+  })  
   const handleClick = () => {
     setIsLoading(true);
     fetch("http://localhost:8080/create_preference", {
