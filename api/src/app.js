e7438fc851909305d6b48491001fb16a27ca16a1
gastondvoskin <dvoskingaston@gmail.com>
2023-07-02 19:28:40 -0300

    deploying back and front 6

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*' ); /* https://viandaexpress.vercel.app http://localhost:3000 */
+  res.header('Access-Control-Allow-Origin', 'https://viandaexpress.vercel.app' ); /* https://viandaexpress.vercel.app http://localhost:3000 */
   res.header("Access-Control-Allow-Credentials", "true"); 
   res.header(
     "Access-Control-Allow-Headers",
