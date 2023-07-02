fe664a94e19ede74fef609cdd50b829a359e0e23
santilagos <sntlgs@gmail.com>
2023-07-02 17:41:56 -0300

    mas limpieza

--- api/src/controllers/itemControllers/postItemController.js
+++ api/src/controllers/itemControllers/postItemController.js
@@ -3,7 +3,7 @@ const {updateCartTotalPrice} = require("./updateCartTotalPrice")
 
 const postItemController = async (userId,foodId,quantity,price) => {
     // Buscar el carrito del usuario con estado "PENDIENTE"
-    console.log("dentro del controller")
+
     let cart = await Order.findOne({
         where: {
           UserId: userId,
@@ -13,7 +13,6 @@ const postItemController = async (userId,foodId,quantity,price) => {
   
       // Si no existe un carrito, se crea uno nuevo
       if (!cart) {
-        console.log("noHay order creo una")
         cart = await Order.create({
           UserId: userId,
           total_price: 0,
@@ -22,7 +21,6 @@ const postItemController = async (userId,foodId,quantity,price) => {
       }
   
       // Crear el nuevo artículo y asociarlo al carrito
-      console.log("por crear")
       const item = await Item.create({
         OrderId: cart.id,
         FoodId: foodId,
@@ -31,9 +29,8 @@ const postItemController = async (userId,foodId,quantity,price) => {
         amount: price * quantity,
       });
 
-      console.log("cread")
       await updateCartTotalPrice(cart.id);
-      console.log("Se actualiza")
+
       return item;
 }
 
