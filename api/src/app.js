5b71100f61f6c1de410795c7d3904dd7146d9636
gastondvoskin <dvoskingaston@gmail.com>
2023-07-02 19:25:29 -0300

    deploying back and front 5

--- api/src/app.js
+++ api/src/app.js
@@ -11,17 +11,16 @@ const app = express();
 // MIDDLEWARES
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
 // MIDDLEWARE: CORS CONFIGURATION
-/* app.use(cors()); */
+app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://viandaexpress.vercel.app' ); /* https://viandaexpress.vercel.app http://localhost:3000 */
-  res.header("Access-Control-Allow-Credentials", "true"); /* ?? */
+  res.header('Access-Control-Allow-Origin', '*' ); /* https://viandaexpress.vercel.app http://localhost:3000 */
+  res.header("Access-Control-Allow-Credentials", "true"); 
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  ); /* ?? */
+  ); 
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
