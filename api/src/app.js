efc01b760a74ac4cdf2261842f63fbb53b61af25
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2023-07-03 21:03:39 -0300

    Configuaracion de peticiones para presentacion

--- api/src/app.js
+++ api/src/app.js
@@ -15,12 +15,12 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*' ); /* https://viandaexpress.vercel.app, http://localhost:5173 */
-  res.header("Access-Control-Allow-Credentials", "true"); 
+  res.header('Access-Control-Allow-Origin', '*'); /* https://viandaexpress.vercel.app, http://localhost:5173 */
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  ); 
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
