5e35e45e021abdaea35270687f44e449f3339f63
Gabriel-682 <116679204+Gabriel-682@users.noreply.github.com>
2023-07-04 08:34:34 -0300

    Detail handle items DB

--- client/src/clientViews/Detail/Detail.jsx
+++ client/src/clientViews/Detail/Detail.jsx
@@ -10,7 +10,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Detail() {
   const { id } = useParams();
-  const {isAuthenticated } = useAuth0();
+  const {isAuthenticated, user } = useAuth0();
   const [isItem,setIsItem]=useState(false);
   const allItems=useSelector((state)=>state.foodsReducer.orderItems);
   const dispatch = useDispatch();
@@ -44,11 +44,37 @@ export default function Detail() {
       alert('¡Cuidado! Logueate antes de agregar productos a tu carrito de compras. ¡Gracias!')
     }else{
       if(isItem){
-        setIsItem(false),
+        setIsItem(false);
         dispatch(deleteItemActions(id));
+        //-------------------------
+        const bodyDelete = {
+          userEmail: user?.email,
+          FoodId: id,
+        };
+        console.log("DeleteAction", bodyDelete);
+        axios
+          .delete("/item", { data: bodyDelete })
+          .catch((error) => console.log(error));
+        //-------------------------
       }else{
         setIsItem(true);
-        dispatch(addItemsActions({id,name:foodDetail?.name,image:foodDetail?.image,final_price:foodDetail?.final_price}))
+        dispatch(
+          addItemsActions({
+            id,
+            name: foodDetail?.name,
+            image: foodDetail?.image,
+            final_price: foodDetail?.final_price,
+          })
+        );
+        //-------------------------
+        const bodyAdd = {
+          userEmail: user?.email,
+          FoodId: id,
+          quantity: 1,
+          final_price: foodDetail?.final_price,
+        };
+        axios.post("/item", bodyAdd).catch((error) => console.log(error));
+        //-------------------------
       }
     }
   }
