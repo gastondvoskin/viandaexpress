cad9f5b9f4a62ca1cb8a613b4e0c26abbb4e950a
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2023-07-04 21:31:44 -0300

    Carrito de compras

--- client/src/clientViews/ShoppingCar/ShoppingCar.jsx
+++ client/src/clientViews/ShoppingCar/ShoppingCar.jsx
@@ -6,8 +6,8 @@ import Footer from "../../clientComponents/ShoppingCar/Footer.jsx";
 import InternalProvider from "../../clientComponents/ShoppingCar/ContextProvider.jsx";
 import { SpinnerCircular } from "spinners-react";
 import { useDispatch, useSelector } from "react-redux";
-import { useAuth0 } from "@auth0/auth0-react"; 
-import {getUserDetailAction} from '../../redux/userSlice' 
+import { useAuth0 } from "@auth0/auth0-react";
+import { getUserDetailAction } from "../../redux/userSlice";
 import { getPendingOrderAction } from "../../redux/shopingCartSlice";
 
 // REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
@@ -15,19 +15,19 @@ import { getPendingOrderAction } from "../../redux/shopingCartSlice";
 initMercadoPago("TEST-6062b4aa-0752-422b-b693-2282b2ede839");
 
 const ShoppingCar = () => {
-  const {user}=useAuth0();
-  const allItems=useSelector((state)=>state.foodsReducer.orderItems);
-  let total=0;
+  const { user } = useAuth0();
+  const allItems = useSelector((state) => state.foodsReducer.orderItems);
+  let total = 0;
   const [preferenceId, setPreferenceId] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [orderData, setOrderData] = useState(allItems);
-  const dispatch=useDispatch();
-  const currentUser=useState(state=>state.usersReducer.userDetail);
-  useEffect(()=>{
-    if(!currentUser.id){
-      dispatch(getUserDetailAction(user?.email))
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.usersReducer.userDetail);
+  useEffect(() => {
+    if (!currentUser.id) {
+      dispatch(getUserDetailAction(user?.email));
     }
-  },[dispatch])
+  }, [dispatch]);
   const handleClick = () => {
     setIsLoading(true);
     //CAMBIAR A AXIOS
@@ -49,8 +49,8 @@ const ShoppingCar = () => {
       })
       .finally(() => {
         setIsLoading(false);
-      })
-      dispatch(getPendingOrderAction(currentUser.id))
+      });
+    dispatch(getPendingOrderAction(currentUser.id));
   };
 
   const renderSpinner = () => {
