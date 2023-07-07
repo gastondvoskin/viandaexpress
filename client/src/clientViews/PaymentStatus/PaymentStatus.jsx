b470dde55681522c6fceb37459dc7df1bf8e57ad
Gabriel-682 <116679204+Gabriel-682@users.noreply.github.com>
2023-07-07 00:51:35 -0300

    Put Order Controller Created

--- client/src/clientViews/PaymentStatus/PaymentStatus.jsx
+++ client/src/clientViews/PaymentStatus/PaymentStatus.jsx
@@ -1,16 +1,31 @@
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const PaymentStatus = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const queryParamValue = queryParams.get("status");
+  const { user } = useAuth0();
+  const userEmail = user?.email;
+  let status = queryParams.get("status");
+  const merchant_order_id = queryParams.get("merchant_order_id");
+  const payment_id = queryParams.get("payment_id");
 
-  console.log("PaymentComponent: ", queryParamValue);
+  if (status === "approved") status = "APROBADO";
+
+  const bodyPutOrder = {
+    userEmail,
+    status,
+    merchant_order_id,
+    payment_id,
+  };
+
+  axios.put("/order", bodyPutOrder);
 
   return (
     <>
       <div>Payment</div>
-      <div>Status: {queryParamValue}</div>
+      <div>Status: {status}</div>
     </>
   );
 };
