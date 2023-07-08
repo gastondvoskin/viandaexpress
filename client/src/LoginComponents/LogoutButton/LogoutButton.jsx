1b9eb808015fac004c6b91f89e623b2333c76f26
MagnoRay <mraymundodiaz@gmail.com>
2023-07-07 19:25:22 -0500

    Update Nav

--- client/src/LoginComponents/LogoutButton/LogoutButton.jsx
+++ client/src/LoginComponents/LogoutButton/LogoutButton.jsx
@@ -1,10 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import styles from "./LogoutButton.module.css";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button 
+    <button className={styles.btnuser}
       /* onClick={() => logout( {returnTo: window.location.origin} )} */
       onClick={() => logout( {returnTo: "https://viandaexpress.vercel.app/"} )}
     >Cerrar sesión 
