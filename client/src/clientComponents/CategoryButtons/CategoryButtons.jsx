5718dcfe8e84fb439865b56abda4e57a7847685d
gastondvoskin <dvoskingaston@gmail.com>
2023-07-11 16:35:27 -0300

    Restyled categories buttons

--- client/src/clientComponents/CategoryButtons/CategoryButtons.jsx
+++ client/src/clientComponents/CategoryButtons/CategoryButtons.jsx
@@ -1,70 +1,84 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { activeFilteredFoodsAction, filterByCategoryAction, setCategoryAction, setCurrentPageAction, setDietAction, setOrderAction } from '../../redux/foodActions';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  activeFilteredFoodsAction,
+  filterByCategoryAction,
+  setCategoryAction,
+  setCurrentPageAction,
+  setDietAction,
+  setOrderAction,
+} from "../../redux/foodActions";
 import styles from "./CategoryButtons.module.css";
+import pastasImage from "../../assets/categories/pastas.jpg";
+import carnesImage from "../../assets/categories/meat.jpg";
+import saladImage from "../../assets/categories/salad.jpeg";
 
 
 const CategoryButtons = () => {
-    const allFoods  = useSelector((state) => state.foodsReducer.allFoods);
-    const dispatch = useDispatch()
+  const allFoods = useSelector((state) => state.foodsReducer.allFoods);
+  const dispatch = useDispatch();
 
-    const handleClick = (e) =>{
-        const value = e.target.value
-        const filteredFoods = handlerFilterCategory(value)
-        dispatch(filterByCategoryAction(filteredFoods))
-        dispatch(setOrderAction(''))
-        dispatch(activeFilteredFoodsAction(true))
-        dispatch(setCurrentPageAction(1))
-        dispatch(setCategoryAction(value))
-        dispatch(setDietAction(''))
-    }
-    
-    const handlerFilterCategory = (value) => {
-        let filteredCategoryFoods;
-        switch (value) {
-            case 'Pastas':
-                filteredCategoryFoods = allFoods.filter(e => e.category === 'Pastas')
-                break;
-            case 'Carnes':
-                filteredCategoryFoods = allFoods.filter(e => e.category === 'Carnes')
-                break;
-            case 'Ensaladas':
-                filteredCategoryFoods = allFoods.filter(e => e.category === 'Ensaladas')
-                break;
-            default:
-                break;
-        }
-        return filteredCategoryFoods;
-    }
+  const handleClick = (e) => {
+    const value = e.target.value;
+    const filteredFoods = handlerFilterCategory(value);
+    dispatch(filterByCategoryAction(filteredFoods));
+    dispatch(setOrderAction(""));
+    dispatch(activeFilteredFoodsAction(true));
+    dispatch(setCurrentPageAction(1));
+    dispatch(setCategoryAction(value));
+    dispatch(setDietAction(""));
+  };
 
+  const handlerFilterCategory = (value) => {
+    let filteredCategoryFoods;
+    switch (value) {
+      case "Pastas":
+        filteredCategoryFoods = allFoods.filter((e) => e.category === "Pastas");
+        break;
+      case "Carnes":
+        filteredCategoryFoods = allFoods.filter((e) => e.category === "Carnes");
+        break;
+      case "Ensaladas":
+        filteredCategoryFoods = allFoods.filter(
+          (e) => e.category === "Ensaladas"
+        );
+        break;
+      default:
+        break;
+    }
+    return filteredCategoryFoods;
+  };
 
   return (
-    <div className={styles.buttonsContainer}>
-        <button
-            className={styles.pastas}
-            onClick={(e) => handleClick(e)}
-            value="Pastas"
-        >
-            Pastas
-        </button>
+    <div className={styles.mainContainer}>
+      <button
+        className={styles.button}
+        onClick={(e) => handleClick(e)}
+        value="Pastas"
+      >
+        <h2 className={styles.text}>Pastas</h2>
+        <img className={styles.image} src={pastasImage} alt="Pastas" />
+      </button>
 
-        <button
-            className={styles.meat}
-            onClick={(e) => handleClick(e)}
-            value="Carnes"
-        >
-            Carnes
-        </button>
+      <button
+        className={styles.button}
+        onClick={(e) => handleClick(e)}
+        value="Carnes"
+      >
+        <h2 className={styles.text}>Carnes</h2>
+        <img className={styles.image} src={carnesImage} alt="Carnes" />
+      </button>
 
-        <button
-            className={styles.salads}
-            onClick={(e) => handleClick(e)}
-            value="Ensaladas"
-        >
-            Ensaladas
-        </button>
-  </div>
-  )
-}
+      <button
+        className={styles.button}
+        onClick={(e) => handleClick(e)}
+        value="Ensaladas"
+      >
+        <h2 className={styles.text}>Ensaladas</h2>
+        <img className={styles.image} src={saladImage} alt="Ensaladas" />
+      </button>
+    </div>
+  );
+};
 
-export default CategoryButtons
+export default CategoryButtons;
