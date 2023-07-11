cf9a5260134062d02e665edafca23016371b161a
gastondvoskin <dvoskingaston@gmail.com>
2023-07-11 18:18:17 -0300

    Deleted Footer.module.css which was throwing an error in console

--- client/src/clientViews/Viandas/Viandas.jsx
+++ client/src/clientViews/Viandas/Viandas.jsx
@@ -24,21 +24,6 @@ const Viandas = () => {
   const { isLoading, user, isAuthenticated } = useAuth0();
   const allItems = useSelector((state) => state.foodsReducer.orderItems);
 
-  // useEffect(() => {
-  //   let body = {};
-  //   if (isAuthenticated) {
-  //     body = {
-  //       name: user?.name,
-  //       email: user?.email,
-  //     };
-  //   } else {
-  //     body = {
-  //       type: "guest",
-  //     };
-  //   }
-  //   axios.post("/user", body).catch((error) => console.log(error));
-  // }, [isAuthenticated, user]);
-
   useEffect(() => {
     if (!allFoods.length) {
       axios.get("/api").then(() => dispatch(getFoods()));
