75f6ff60f1102d1d99177938bcb7903474e363ba
gastondvoskin <dvoskingaston@gmail.com>
2023-07-18 19:36:49 -0300

    Added a star in products with score greater than 4

--- client/src/clientComponents/CardsContainer/CardsContainer.jsx
+++ client/src/clientComponents/CardsContainer/CardsContainer.jsx
@@ -1,24 +1,14 @@
 import Card from "../Card/Card";
-import style from "./CardsContainer.module.css";
+import styles from "./CardsContainer.module.css";
 
 export default function CardsContainer({ currentFoods, allItems, orderId }) {
   return (
-    <div className={style.cardsContainer}>
+    <div className={styles.cardsContainer}>
       {currentFoods.length === 0 ? (
         <p>No se encontraron resultados.</p>
       ) : (
         currentFoods.map(
-          ({
-            id,
-            name,
-            image,
-            initial_price,
-            final_price,
-            category,
-            diets,
-            discount,
-            status,
-          }) => (
+          ({id, name, image, initial_price, final_price, category, diets, discount, status, total_score}) => (
             <Card
               key={id}
               id={id}
@@ -32,10 +22,11 @@ export default function CardsContainer({ currentFoods, allItems, orderId }) {
               diets={diets}
               allItems={allItems}
               orderId={orderId}
+              total_score={total_score}
             />
           )
         )
       )}
     </div>
   );
-};
+}
